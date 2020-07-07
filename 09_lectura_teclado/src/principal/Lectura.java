8e7292075e2c07dfe9c3eff2cf14351ab30a3449
MrIbi <MrIbi@DESKTOP-318JDQD>
2020-07-07 10:41:13 +0200

    commit 1 proyecto 09

---
+++ 09_lectura_teclado/src/principal/Lectura.java
@@ -0,0 +1,14 @@
+package principal;
+
+import java.util.Scanner;
+
+public class Lectura {
+
+	public static void main(String[] args) {
+		
+		// crear un objeto de la clase Scanner y referenciarlo mediante una variable de ese tipo
+		Scanner scanner = new Scanner(System.in);
+
+	}
+
+}
