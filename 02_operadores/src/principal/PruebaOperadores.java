2491539376db251dc70a3356664a8c13a95788bd
MrIbi <MrIbi@DESKTOP-318JDQD>
2020-07-07 10:32:55 +0200

    commit 1 proyecto 02

---
+++ 02_operadores/src/principal/PruebaOperadores.java
@@ -0,0 +1,13 @@
+package principal;
+
+public class PruebaOperadores {
+
+	public static void main(String[] args) {
+		int a = 6;
+		double b = 4;
+		System.out.println(a / b);
+		System.out.println(++a);
+
+	}
+
+}
