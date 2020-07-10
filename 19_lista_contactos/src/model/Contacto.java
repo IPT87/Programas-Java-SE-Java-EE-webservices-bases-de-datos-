ac57d8105ed6a9f11bfffc7f603e9663ec0e8ec5
MrIbi <MrIbi@DESKTOP-318JDQD>
2020-07-10 16:33:30 +0200

    10/07/2020 16:32 added equals() and hashCode()

--- 19_lista_contactos/src/model/Contacto.java
+++ 19_lista_contactos/src/model/Contacto.java
@@ -37,4 +37,36 @@ public class Contacto {
 		this.telefono = telefono;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + telefono;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Contacto))
+			return false;
+		Contacto other = (Contacto) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (telefono != other.telefono)
+			return false;
+		return true;
+	}
+	
 }
