d0dd7d99ebadda1fd07f2c79fb726a5fab70cd2c
MrIbi <MrIbi@DESKTOP-318JDQD>
2020-09-09 17:28:05 +0200

    09/09/2020  17:28

--- 37_cajero_completo/src/presentacion/graficos/JVentanaPrincipal.java
+++ 37_cajero_completo/src/presentacion/graficos/JVentanaPrincipal.java
@@ -11,20 +11,17 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class JVentanaPrincipal extends JFrame {
 
 	private JPanel contentPane;
 	private CajeroService cs = new CajeroService();
 	private int numeroCuenta;
-	private JTextField tfCantidadIngresar;
-	private JTextField tfCantidadExtraer;
-	private JTextField tfTransferencia;
-	private JLabel jlCantidadIngresar;
-	private JLabel jlCantidadExtraer;
 	private JLabel jlCantidadTransferencia;
-	private JTextField tfCuentaDestino;
-	private JLabel jlCuentaDestino;
 	private JButton jbSalir;
 
 	/**
@@ -58,36 +55,57 @@ public class JVentanaPrincipal extends JFrame {
 		JButton jbIngresar = new JButton("Ingresar");
 		jbIngresar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				double cantidad = Double.parseDouble(tfCantidadIngresar.getText());
-				cs.ingresar(cantidad, numeroCuenta);
-				tfCantidadIngresar.setText("");
+				try {
+					String insertCantidad = JOptionPane.showInputDialog(JVentanaPrincipal.this, "Cantidad");
+					if (insertCantidad != null) {
+						double cantidad = Double.parseDouble(insertCantidad);
+						cs.ingresar(cantidad, numeroCuenta);
+					}
+				} catch (Exception exc) {
+					JOptionPane.showMessageDialog(JVentanaPrincipal.this, "Cantidad incorrecta!");
+				}
 			}
 		});
-		jbIngresar.setBounds(34, 66, 119, 28);
+		jbIngresar.setBounds(34, 81, 119, 28);
 		contentPane.add(jbIngresar);
 		
 		JButton jbExtraer = new JButton("Extraer");
 		jbExtraer.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				double cantidad = Double.parseDouble(tfCantidadExtraer.getText());
-				cs.extraer(cantidad, numeroCuenta);
-				tfCantidadExtraer.setText("");
+				try {
+					String insertCantidad = JOptionPane.showInputDialog(JVentanaPrincipal.this, "Cantidad");
+					if (insertCantidad != null) {
+						double cantidad = Double.parseDouble(insertCantidad);
+						cs.extraer(cantidad, numeroCuenta);
+					}
+				} catch (Exception exception) {
+					JOptionPane.showMessageDialog(JVentanaPrincipal.this, "Cantidad incorrecta!");
+				}
 			}
 		});
-		jbExtraer.setBounds(235, 66, 119, 28);
+		jbExtraer.setBounds(34, 119, 119, 28);
 		contentPane.add(jbExtraer);
 		
 		JButton jbTransferencia = new JButton("Transferencia");
 		jbTransferencia.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				double cantidad = Double.parseDouble(tfTransferencia.getText());
-				int cuentaDestino = Integer.parseInt(tfCuentaDestino.getText());
-				cs.transferencia(numeroCuenta, cuentaDestino, cantidad);
-				tfTransferencia.setText("");
-				tfCuentaDestino.setText("");
+				try {
+					String insertCantidad = JOptionPane.showInputDialog(JVentanaPrincipal.this, "Cantidad");
+					if (insertCantidad != null) {
+						double cantidad = Double.parseDouble(insertCantidad);
+						String insertCuenta = JOptionPane.showInputDialog(JVentanaPrincipal.this, "Cuenta destino");
+						if (insertCuenta != null) {
+							int cuentaDestino = Integer.parseInt(insertCuenta);
+							cs.transferencia(numeroCuenta, cuentaDestino, cantidad);
+						}
+					}
+				} catch (Exception ex) {
+					JOptionPane.showMessageDialog(JVentanaPrincipal.this, "Cantidad o numero de cuenta incorrecto!");
+				}
+				
 			}
 		});
-		jbTransferencia.setBounds(437, 157, 119, 28);
+		jbTransferencia.setBounds(34, 195, 119, 28);
 		contentPane.add(jbTransferencia);
 		
 		JButton jbMovimientos = new JButton("Movimientos");
@@ -109,46 +127,27 @@ public class JVentanaPrincipal extends JFrame {
 				JOptionPane.showConfirmDialog(JVentanaPrincipal.this, cs.obtenerSaldo(numeroCuenta) + "€", "Saldo", JOptionPane.CLOSED_OPTION);
 			}
 		});
-		jbObtenerSaldo.setBounds(235, 157, 119, 28);
+		jbObtenerSaldo.setBounds(34, 42, 119, 28);
 		contentPane.add(jbObtenerSaldo);
 		
-		tfCantidadIngresar = new JTextField();
-		tfCantidadIngresar.setBounds(34, 37, 119, 19);
-		contentPane.add(tfCantidadIngresar);
-		tfCantidadIngresar.setColumns(10);
-		
-		tfCantidadExtraer = new JTextField();
-		tfCantidadExtraer.setColumns(10);
-		tfCantidadExtraer.setBounds(235, 37, 119, 19);
-		contentPane.add(tfCantidadExtraer);
-		
-		tfTransferencia = new JTextField();
-		tfTransferencia.setColumns(10);
-		tfTransferencia.setBounds(437, 37, 119, 19);
-		contentPane.add(tfTransferencia);
-		
-		jlCantidadIngresar = new JLabel("Cantidad");
-		jlCantidadIngresar.setBounds(75, 10, 78, 13);
-		contentPane.add(jlCantidadIngresar);
-		
-		jlCantidadExtraer = new JLabel("Cantidad");
-		jlCantidadExtraer.setBounds(276, 14, 78, 13);
-		contentPane.add(jlCantidadExtraer);
-		
-		jlCantidadTransferencia = new JLabel("Cantidad");
-		jlCantidadTransferencia.setBounds(478, 14, 78, 13);
+		jlCantidadTransferencia = new JLabel("Elige operaciòn:");
+		jlCantidadTransferencia.setFont(new Font("Tahoma", Font.BOLD, 14));
+		jlCantidadTransferencia.setBackground(Color.WHITE);
+		jlCantidadTransferencia.setBounds(247, 10, 205, 21);
 		contentPane.add(jlCantidadTransferencia);
 		
-		tfCuentaDestino = new JTextField();
-		tfCuentaDestino.setColumns(10);
-		tfCuentaDestino.setBounds(437, 97, 119, 19);
-		contentPane.add(tfCuentaDestino);
-		
-		jlCuentaDestino = new JLabel("Cuenta destino");
-		jlCuentaDestino.setBounds(463, 74, 93, 13);
-		contentPane.add(jlCuentaDestino);
-		
 		jbSalir = new JButton("Salir");
+		Color c = jbSalir.getBackground();
+		jbSalir.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				jbSalir.setBackground(Color.RED);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				jbSalir.setBackground(c);
+			}
+		});
 		jbSalir.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				int resp = JOptionPane.showConfirmDialog(JVentanaPrincipal.this, "Desea salir", "Ventana salida", JOptionPane.YES_NO_OPTION);
