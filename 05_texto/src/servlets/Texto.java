5d7dd667fe4ab06862b36be1279b926e079509e3
MrIbi <MrIbi@DESKTOP-318JDQD>
2020-11-16 17:39:20 +0100

    16/11/2020  17:39

---
+++ 05_texto/src/servlets/Texto.java
@@ -0,0 +1,33 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Texto
+ */
+@WebServlet("/Texto")
+public class Texto extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		
+		int size = Integer.parseInt(request.getParameter("size"));
+		
+		out.println("<h" + size + ">Esto es un texto<h" + size + "><br/>");
+		out.println("</body></html>");
+	}
+
+}
