95ab7b2e627d33e45e5e7f239418b145ad048ea8
MrIbi <MrIbi@DESKTOP-318JDQD>
2020-11-19 17:25:11 +0100

    19/11/2020  17:25

---
+++ 25_buscador_Spring/src/model/Item.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Item {
+	private String titulo;
+	private String url;
+	private String[] seo;
+	private String descripcion;
+	
+	public Item(String titulo, String url, String[] seo, String descripcion) {
+		super();
+		this.titulo = titulo;
+		this.url = url;
+		this.seo = seo;
+		this.descripcion = descripcion;
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	public String getUrl() {
+		return url;
+	}
+	
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	public String[] getSeo() {
+		return seo;
+	}
+	
+	public void setSeo(String[] seo) {
+		this.seo = seo;
+	}
+	
+	public String getDescripcion() {
+		return descripcion;
+	}
+	
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+}
