9f272ca07cadb56abf97089a64338507b7f98c6c
MrIbi <MrIbi@DESKTOP-318JDQD>
2021-01-17 18:36:42 +0100

    17/01/2021  18:36

---
+++ 06_ficha_rest/src/config/MvcConfig.java
@@ -0,0 +1,14 @@
+package config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@ComponentScan(basePackages = {"controller"})
+@Configuration
+public class MvcConfig {
+	
+	
+	
+}
